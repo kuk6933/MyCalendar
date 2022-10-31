c895f89fb887249469ebd45a23c13bed85c3fe69
HyeongSeok <gudtjr051911@gmail.com>
2022-10-31 17:51:03 +0900

    temporary json file

--- Mycalendar/Mycalendar/Model/Model.swift
+++ Mycalendar/Mycalendar/Model/Model.swift
@@ -9,4 +9,43 @@ import Foundation
 
 struct Model {
     var memoList = ["Memo1", "Memo2", "Memo3", "Memo4", "Memo5"]
+    
+    init() {
+        loadMemo()
+    }
+    
+    func load() -> Data? {
+        let fileNm: String = "memo"
+        
+        let extensionType = "json"
+        
+        guard let fileLocation = Bundle.main.url(forResource: fileNm, withExtension: extensionType) else { return nil }
+        
+        do {
+            let data = try Data(contentsOf: fileLocation)
+            return data
+        } catch {
+            return nil
+        }
+    }
+    
+    func loadMemo() {
+        guard
+            let jsonData = load(),
+            let dictData = String(data: jsonData, encoding: .utf8)
+        else {
+            return
+        }
+        print("결과: \(dictData)")
+    }
+}
+
+struct MemoList: Codable {
+    let totalCount: Int
+    let memos: [Memo]
+}
+
+struct Memo: Codable {
+    let title: String
+    let content: String
 }
